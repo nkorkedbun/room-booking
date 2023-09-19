d79739c57a436146090c2a767fc4e93782ba9ac1
Nutthawut Korkedbun <nkorkedbun@palo-it.com>
2023-09-19 09:32:45 +0700

    update app bar

--- mobile/lib/common/booking_room_app_bar.dart
+++ mobile/lib/common/booking_room_app_bar.dart
@@ -8,7 +8,6 @@ class BookingRoomAppBar extends StatelessWidget with PreferredSizeWidget {
     return Stack(
       children: [
         Container(
-          padding: const EdgeInsets.fromLTRB(22, 20, 22, 16),
           decoration: const BoxDecoration(
             borderRadius: BorderRadius.only(
               bottomLeft: Radius.circular(20),
@@ -17,52 +16,65 @@ class BookingRoomAppBar extends StatelessWidget with PreferredSizeWidget {
             image: DecorationImage(
               image: AssetImage('assets/app_bar_bg.png'),
               fit: BoxFit.fitHeight,
-              alignment: Alignment.bottomCenter,
             ),
           ),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  InkWell(
-                    onTap: () {},
-                    child: const Icon(
-                      Icons.arrow_back,
-                      color: Colors.white,
-                      size: 20,
-                    ),
-                  ),
-                  const Spacer(),
-                  TextButton(
-                      onPressed: () {},
-                      child: const Text(
-                        "Logout",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                            fontFamily: 'Open Sans',
-                            fontWeight: FontWeight.w400,
-                            height: 1.5),
-                      )),
-                ],
-              ),
-              Row(
-                children: [
-                  Text(
-                    title,
-                    style: const TextStyle(
-                      fontFamily: 'Montserrat',
-                      fontSize: 28,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.white,
-                      height: 1.5,
-                    ),
-                  )
-                ],
-              )
-            ],
-          ),
         ),
+        Container(
+          color: const Color.fromRGBO(
+              0, 0, 0, 0.5), // Semi-transparent overlay color
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(22, 0, 22, 16),
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        print("Back button pressed");
+                        print(Navigator.canPop(context));
+                        Navigator.pop(context);
+                      },
+                      child: const Icon(
+                        Icons.arrow_back,
+                        color: Colors.white,
+                        size: 20,
+                      ),
+                    ),
+                    const Spacer(),
+                    TextButton(
+                        onPressed: () => {Navigator.pop(context)},
+                        child: const Text(
+                          "Logout",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontFamily: 'Open Sans',
+                              fontWeight: FontWeight.w400,
+                              height: 1.5),
+                        )),
+                  ],
+                ),
+                const Spacer(),
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontFamily: 'Montserrat',
+                    fontSize: 28,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                    height: 1.5,
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+              ],
+            ),
+          ),
+        )
       ],
     );
   }
